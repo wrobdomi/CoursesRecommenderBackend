9fa0d9cacc15cd1168697700323fe9e48c0a1d69
wrobdomi <wrobel.dominik.world@gmail.com>
2020-03-18 00:35:39 +0100

    upgrade

---
+++ demo_18_03__00_35/src/main/java/com/example/demo/datalayer/repository/CourseRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.datalayer.repository;
+
+import com.example.demo.datalayer.entity.CourseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends PagingAndSortingRepository<CourseEntity, Long> {
+
+    CourseEntity findByName(String name);
+    CourseEntity findByCourseId(String courseId);
+    CourseEntity findById(long id);
+    Page<CourseEntity> findByCategory(String category, Pageable pageable);
+    List<CourseEntity> findAllByCategory(String category);
+
+}
--- demo_syllabus_url_added/demo/src/main/java/com/example/demo/servicelayer/RatingService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.servicelayer;
-
-import com.example.demo.shared.dto.RatingDto;
-
-public interface RatingService  {
-
-    RatingDto createRating(RatingDto ratingDto);
-
-}
