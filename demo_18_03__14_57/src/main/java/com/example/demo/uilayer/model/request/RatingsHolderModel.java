37728c2b76ab4cfed86ff8adffe8e13716408508
wrobdomi <wrobel.dominik.world@gmail.com>
2020-03-18 14:57:46 +0100

    demo

---
+++ demo_18_03__14_57/src/main/java/com/example/demo/uilayer/model/request/RatingsHolderModel.java
@@ -0,0 +1,15 @@
+package com.example.demo.uilayer.model.request;
+
+import java.util.List;
+
+public class RatingsHolderModel {
+    private List<RatingModel> ratings;
+
+    public List<RatingModel> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<RatingModel> ratings) {
+        this.ratings = ratings;
+    }
+}
